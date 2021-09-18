954d89d2129c7da7313aefb36e48117969e14223
marko2000 <mmarkom2000@gmail.com>
2021-09-18 19:30:04 +0200

    Changes in main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-$(document).ready(function() {
-    $("#playbtn").click(function(){
-        let vred = $("#textinput").val();
-        let vred1 = vred.split(" ");
-        for(let i=0; i<vred1.length(); i++){
-            console.log(vred1[i]);
-        }
-    }); 
-});
+// $(document).ready(function() {
+//     $("#playbtn").click(function(){
+//         let vred = $("#textinput").val();
+//         let vred1 = vred.split(" ");
+//         for(let i=0; i<vred1.length(); i++){
+//             console.log(vred1[i]);
+//         }
+//     }); 
+// });
 
+let function1 = () => {
+    let vred = document.getElementById("textinput").value;
+    let vred1 = vred.split(" ")
+    vred1.map(elm => console.log(elm))
+}
